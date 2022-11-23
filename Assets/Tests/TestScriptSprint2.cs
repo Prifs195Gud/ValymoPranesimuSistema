c2d6c5a288a8e921332ade2ff613c1980afc45c5
Rolandas Romanovskis <65602933+rolik55@users.noreply.github.com>
2022-11-23 15:53:01 +0200

    2 ir 3 sprintų testai

---
+++ Assets/Tests/TestScriptSprint2.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestScriptSprint2
+{
+	[Test]
+	public void SekmingasNuomosPlatformosPridejimas()
+	{
+
+	}
+
+	[Test]
+	public void NesekmingasNuomosPlatformosPridejimas()
+	{
+
+	}
+
+	[Test]
+	public void SekmingaRegistracija()
+	{
+		
+	}
+
+	[Test]
+	public void NesekmingaRegistracija()
+	{
+		
+	}
+
+	[Test]
+	public void SekmingasPrisijungimas()
+	{
+
+	}
+
+	[Test]
+	public void NesekmingasPrisijungimas()
+	{
+
+	}
+}
